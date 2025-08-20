d58614c93c6c7f7a521b24954d4fa8d914089132
ULTIepic <152503512+ULTIepic@users.noreply.github.com>
2025-08-20 18:12:39 -0400

    Added my pwa uwu
    pwa

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'pwa-cache-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png',
+  [
+  "https://unpkg.com/react@18/umd/react.production.min.js",
+  "https://unpkg.com/react-dom@18/umd/react-dom.production.min.js",
+  "https://unpkg.com/@babel/standalone/babel.min.js",
+  "https://unpkg.com/three@0.160.0/build/three.min.js",
+  "https://unpkg.com/@react-three/fiber/dist/react-three-fiber.umd.js"
+]
+].flat();
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME ? caches.delete(k) : null)))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request, { ignoreSearch: true }).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
